f583d3cf3d949f76fab57a34bb26968194c22d52
가현 <102145605+rkgus24@users.noreply.github.com>
2023-07-05 15:38:46 +0900

    온습도 센서 + ProMicro final code

---
+++ first/serial_connect_final.ino
@@ -0,0 +1,37 @@
+#include "DHT.h";
+#define DHTPIN A1
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+    // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  Serial.print("Humidity:");
+  Serial.println(h);
+  Serial.print("Temperature:");
+  Serial.println(t);
+  //Serial.print(F("°F  Heat index: "));
+  //Serial.print(hic);
+  //Serial.print(F("°C "));
+  //Serial.print(hif);
+  //Serial.println(F("°F"));
+}
